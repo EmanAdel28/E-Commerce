ec908be6beb910a0d644e7ad1791be49ac42db02
AmiraAdel28 <amira.adel282001@gmail.com>
2025-04-23 23:51:46 +0200

    UnitOfWork

---
+++ Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+
+        IGenericReposetory<TEntity, TKey> GetRepository<TEntity, TKey>() where   TEntity : ModelBase<TKey>;
+    }
+}
