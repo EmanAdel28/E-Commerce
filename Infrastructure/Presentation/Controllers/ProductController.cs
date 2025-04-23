d69215f66286da98a36cef653d9307057f59e59a
AmiraAdel28 <amira.adel282001@gmail.com>
2025-04-24 01:29:53 +0200

    Product Controller

---
+++ Infrastructure/Presentation/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstraction;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTO_S;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController(IServicesManager servicesManager) :ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetAllProducts()
+        {
+            var products = await servicesManager.ProductServices.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IEnumerable<BrandDto>>> GetAllBrands()
+        {
+            var brands = await servicesManager.ProductServices.GetAllBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("Types")]
+        public async Task<ActionResult<IEnumerable<TypeDto>>> GetAllTypes()
+        {
+            var Types = await servicesManager.ProductServices.GetAllTypesAsync();
+            return Ok(Types);
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProductById(int id)
+        {
+            var product = await servicesManager.ProductServices.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
